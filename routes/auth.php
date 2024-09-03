75af71f1859a1d3f98981b995beb09fec20004e8
anxdre <anxdre@protonmail.com>
2024-09-03 12:51:53 +0700

    finish : migrating to custom auth

--- routes/auth.php
+++ routes/auth.php
@@ -1,14 +1,14 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\Auth_Deprecated\AuthenticatedSessionController;
+use App\Http\Controllers\Auth_Deprecated\ConfirmablePasswordController;
+use App\Http\Controllers\Auth_Deprecated\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth_Deprecated\EmailVerificationPromptController;
+use App\Http\Controllers\Auth_Deprecated\NewPasswordController;
+use App\Http\Controllers\Auth_Deprecated\PasswordController;
+use App\Http\Controllers\Auth_Deprecated\PasswordResetLinkController;
+use App\Http\Controllers\Auth_Deprecated\RegisteredUserController;
+use App\Http\Controllers\Auth_Deprecated\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
