ada5aa1bede770e19bea8063916ebc7b2273bae3
mantan <andre@itpi.co.id>
2026-06-22 20:51:46 +0700

    fix dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,10 +6,10 @@ if [ ! -f .env ]; then
     cp .env.example .env
 fi
 
-if ! grep -q "^APP_KEY=" .env || [ "$(grep "^APP_KEY=" .env | cut -d= -f2)" = "" ]; then
-    echo "Generating APP_KEY"
-    php artisan key:generate --force
-fi
+#if ! grep -q "^APP_KEY=" .env || [ "$(grep "^APP_KEY=" .env | cut -d= -f2)" = "" ]; then
+#    echo "Generating APP_KEY"
+#    php artisan key:generate --force
+#fi
 
 echo "Creating storage link"
 php artisan storage:link --force 2>/dev/null || true
